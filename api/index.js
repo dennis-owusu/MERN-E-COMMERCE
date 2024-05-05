b04918da3bd08e8164646f4e759c65912e63dab8
dennis-owusu <kwesimodestygh111@gmail.com>
2024-05-04 22:17:56 -0800

    Connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -5,13 +5,20 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
 dotenv.config()
 
+const PORT = process.env.PORT || 3000
 const app = express()
 
-const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+mongoose.connect(process.env.MONGO_URI).then(()=> {
+    console.log("Mongodb connected")
+})
 
 app.listen(PORT, (req, res) =>{
     console.log(`Server is running on port ${PORT}`)
