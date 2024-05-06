d6e60a3458cd111ef26532da05493b8ff99a205c
dennis-owusu <kwesimodestygh111@gmail.com>
2024-05-05 19:55:39 -0800

    completed signup and signin logic on the backend, auth model and error handler

--- api/index.js
+++ api/index.js
@@ -3,19 +3,21 @@ import express from 'express';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
+import userRoute from './routes/auth.route.js'
 dotenv.config()
 
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookieParser()) 
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
+app.use('/api/auth', userRoute)
 mongoose.connect(process.env.MONGO_URI).then(()=> {
     console.log("MongoDB connected")
 })
@@ -23,3 +25,12 @@ mongoose.connect(process.env.MONGO_URI).then(()=> {
 app.listen(PORT, (req, res) =>{
     console.log(`Server is running on port ${PORT}`)
 }) 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
