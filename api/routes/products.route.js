ef12e32b1c403571c8372149a7c80d85f128bf54
dennis-owusu <kwesimodestygh111@gmail.com>
2024-05-12 20:47:37 -0800

    Fetching product route completed

--- api/routes/products.route.js
+++ api/routes/products.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { products } from '../controllers/products.controller.js';
+import { fetchAllProducts, fetchProduct, products } from '../controllers/products.controller.js';
 
 const router = express.Router();
 
-router.post('/products', products)
+router.post('/product', products)
+router.get('/products/:productId', fetchProduct)
+router.get('/allproducts', fetchAllProducts)
 
 export default router
