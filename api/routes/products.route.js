b94275f645eed9e305694986d40b713dcf86afda
dennis-owusu <kwesimodestygh111@gmail.com>
2024-05-14 23:10:05 -0800

    Single product page created

--- api/routes/products.route.js
+++ api/routes/products.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { fetchAllProducts, products } from '../controllers/products.controller.js';
+import { fetchAllProducts, product, products } from '../controllers/products.controller.js';
 
 const router = express.Router();
 
 router.post('/product', products)
 router.get('/allproducts', fetchAllProducts)
+router.get('/product/:productId', product)
 
 export default router
